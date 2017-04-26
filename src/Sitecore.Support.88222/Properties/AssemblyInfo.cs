5f6fff5a738b2bb391b97279166b14d0c6a41438
Pavel <piv@sitecore.net>
2017-04-26 14:29:26 +0300

    Fix issue 88222: Rendering parameters template with fields, containing double quotes in values, cannot be edited in Experience Editor

---
+++ src/Sitecore.Support.88222/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.88222")]
+[assembly: AssemblyProduct("Sitecore.Support.88222")]
+[assembly: ComVisible(false)]
